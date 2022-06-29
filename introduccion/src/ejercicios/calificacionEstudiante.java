09037c53322596f6a9dfeb4442dd74843138bba7
Mauricio Carvajal <carvabmx@gmail.com>
2022-06-28 22:08:28 -0500

    Ejercicico: Creacion ejerciccio con metodos Math. MC

---
+++ introduccion/src/ejercicios/calificacionEstudiante.java
@@ -0,0 +1,26 @@
+//La calificacion final de un estudiante de informatica se calcula con base a calificaciones de cuatro aspectos de sus rendicminto academico: participacion, primer examen parcial, sugundo examen parcial y examen final. Sabiendo que las calificaciones anteriores entran a la calificacion final con ponderaciones del 10%, 25%, 25% y 40%, Hacer un programa que calcule e imprima la calificacion final obtenida por un estudiante.
+
+//The final grade of a computer science student is calculated based on grades of four aspects of their academic performance: participation, first partial exam, second partial exam and final exam. Knowing that the previous grades enter the final grade with weights of 10%, 25%, 25% and 40%, make a program that calculates and prints the final grade obtained by a student.
+
+import java.util.Scanner;
+public class calificacionEstudiante {
+    public static void main(String[] args) throws Exception {
+        Scanner score = new Scanner(System.in);
+        float participation, exam1, exam2, exam3, total;
+
+        System.out.println("Type the carticipation: ");
+        participation = score.nextFloat();
+
+        System.out.println("Type the exam 1: ");
+        exam1 = score.nextFloat();
+
+        System.out.println("Type the exam 2: ");
+        exam2 = score.nextFloat();
+
+        System.out.println("Type the cexam 3: ");
+        exam3 = score.nextFloat();
+
+        total = (participation * 0.10f) + (exam1 * 0.25f) + (exam2 * 0.25f) + (exam3 * 0.40f);
+        System.out.println("The total score is: " + total);
+    }
+}
---
+++ introduccion/src/ejercicios/salarioMensual.java
@@ -0,0 +1,22 @@
+//Una campañia de venta de carros usados, paga a una persona de ventas un salario de $1000 mensuales, mas comision de $150 por cada carro vendido, mas 5% del valor de la venta por carro. Cada mes el capturista de la empresa ingresa en la computadora los datos pertinetes. Hecer un programa que calcule e imprima el salario mesual de un vendedor.
+
+//A used car sales campaign pays a sales person a salary of $1,000 per month, plus a commission of $150 for each car sold, plus 5% of the value of the sale per car. Each month the company's data entry keyer enters the pertinent data into the computer. Write a program that calculates and prints the monthly salary of a salesman.
+
+import java.util.Scanner;
+public class salarioMensual {
+    public static void main(String[] args) throws Exception {
+        Scanner salary = new Scanner(System.in);
+       
+        final int salaryMonth = 1000;
+
+        System.out.println("Enter the number of cars sold: ");
+        int numberCars = salary.nextInt(); 
+
+        System.out.println("Enter the price of the cars: ");
+        float priceCar = salary.nextFloat();
+
+        float total = salaryMonth + (numberCars * 150) + ((priceCar * 0.05f) * numberCars);
+        System.out.println("The total salary is: "+ total);
+    }
+
+}
