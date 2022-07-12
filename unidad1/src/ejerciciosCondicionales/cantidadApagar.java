fe3faeadb5bb82e473c74ac7c185e118dae55dab
Mauricio <carvabmx@gmail.com>
2022-07-12 11:47:14 -0500

    Ejercicio: fechas mes. MC

--- unidad1/src/ejerciciosCondicionales/cantidadApagar.java
+++ unidad1/src/ejerciciosCondicionales/cantidadApagar.java
@@ -3,10 +3,17 @@
 //In a supermarket, a 20% discount is given to customers whose purchase exceeds $300. What will be the amount that a person will pay for their purchase?
 
 import  javax.swing.JOptionPane;
-public class cantidaApagar {
+public class cantidadApagar {
     public static void main(String[] args)throws Exception{
+        double discount, pay;
 
-
-
+        pay = Double.parseDouble(JOptionPane.showInputDialog("Type a payment: "));
+        discount = pay - (pay * 0.20);
+        if(pay > 300){
+            JOptionPane.showMessageDialog(null, "The paiment + discount: " + discount);
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "The discount does't apply: "+ pay);
+        }
     }
 }
