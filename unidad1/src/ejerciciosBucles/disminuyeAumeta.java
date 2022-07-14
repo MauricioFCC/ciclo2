319a2fa602044063d11e0cdcb7db5bbe9106d367
Mauricio Carvajal <carvabmx@gmail.com>
2022-07-13 20:52:20 -0500

    Ejercicio: Condicionales. MC

---
+++ unidad1/src/ejerciciosBucles/disminuyeAumeta.java
@@ -0,0 +1,12 @@
+//Realizar un programa donde los numeros pares entre el 500 y el 1000
+
+public class disminuyeAumeta {
+    public static void main(String[] args)throws Exception {
+        for(int i = 100; i >= 0; i -= 1){ //Se debe de dar un valor de iteracion
+            System.out.println(i);
+        }
+        for(int i = 100; i <= 500; i += 2){ //Se debe de dar un valor de iteracion
+            System.out.println(i);
+        }
+    }
+}
---
+++ unidad1/src/ejerciciosCondicionales/cajeroAutomatico.java
@@ -0,0 +1,13 @@
+//Hacer un programa que simule un cajero automatico con un salario inicial de 1000 usd, con el siguiente menu: 1-Ingrese dinero a la cuenta, 2-Retirar dinero de la cuenta, 3-Salir.
+
+//Make a program that simulates an ATM with an initial salary of 1000 usd, with the following menu: 1-Input money to the account, 2-Withdraw money from the account, 3-Exit.
+
+import javax.swing.JOptionPane;
+
+public class cajeroAutomatico {
+
+    public static void main(String[] args){
+
+        double in, ;
+    }
+}
